84432538350c0c07029ffd19d8c81accddc979ff
mukesh-rara <mukesh@rara.delivery>
2022-11-21 14:17:18 +0530

    Exam request added

--- src/models/driver/ExamRequest.go
+++ src/models/driver/ExamRequest.go
@@ -16,3 +16,15 @@ type Exam struct {
 	CreatedBy             int                `json:"createdBy" bson:"createdBy"`
 	UpdatedBy             int                `json:"updatedBy" bson:"updatedBy"`
 }
+
+type ExamRequest struct {
+	Position              int    `json:"position" bson:"position"`
+	ExamName              string `json:"examName" bson:"examName"`
+	Link                  string `json:"link" bson:"link"`
+	Score                 int    `json:"score" bson:"score"`
+	MaxAttempts           int    `json:"maxAttempts" bson:"maxAttempts"`
+	ExamDuration          int    `json:"examDuration" bson:"examDuration"`
+	AllowTraningAfterDays int    `json:"allowTraningAfterDays" bson:"allowTraningAfterDays"`
+	Status                string `json:"status" bson:"status"`
+	BusinessType          string `json:"businessType" bson:"businessType"`
+}
