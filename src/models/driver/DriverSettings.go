ab3fd07a9ae6789282a18d48f1362291958b3ba9
mukesh-rara <mukesh@rara.delivery>
2022-12-18 20:44:00 +0530

    driver setting model added

---
+++ src/models/driver/DriverSettings.go
@@ -0,0 +1,11 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type DriverSetting struct {
+	Id        primitive.ObjectID `json:"_id" bson:"_id"`
+	Group     string             `json:"group" bson:"group"`
+	MetaLabel string             `json:"metaLabel" bson:"metaLabel"`
+	MetaKey   string             `json:"metaKey" bson:"metaKey"`
+	MetaValue string             `json:"metaValue" bson:"metaValue"`
+}
