d44413d3c40829867b3e781a60bedf584754f6d9
DevYadav-RaRa <dev.yadav@rara.delivery>
2023-01-13 12:38:46 +0530

    [UPDATE]: Vehicle Type Object

--- src/models/driver/VehicleType.go
+++ src/models/driver/VehicleType.go
@@ -3,10 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type VehicleType struct {
-	Id             primitive.ObjectID `json:"_id" bson:"_id"`
-	Name           VehicleName        `json:"name" bson:"name"`
-	Status         string             `json:"status" bson:"status"`
-	Vehicle        []string           `json:"vehicles" bson:"vehicles"`
-	CreatedAt      int64              `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	LastModifiedAt int64              `json:"last_modified_at,omitempty" bson:"last_modified_at,omitempty"`
+	Id             primitive.ObjectID   `json:"_id" bson:"_id"`
+	Name           VehicleName          `json:"name" bson:"name"`
+	Status         string               `json:"status" bson:"status"`
+	Vehicle        []primitive.ObjectID `json:"vehicles" bson:"vehicles"`
+	CreatedAt      int64                `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	LastModifiedAt int64                `json:"last_modified_at,omitempty" bson:"last_modified_at,omitempty"`
 }
