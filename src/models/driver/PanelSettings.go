c574b4b80d63937903ee0f9f75ec51061806292e
DevYadav-RaRa <dev.yadav@rara.delivery>
2023-01-23 17:14:21 +0530

    [UPDATE]: Panel Settings Model

--- src/models/driver/PanelSettings.go
+++ src/models/driver/PanelSettings.go
@@ -3,9 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type PanelSettings struct {
-	Id    primitive.ObjectID `json:"_id" bson:"_id"`
-	Group string             `json:"group" bson:"group"`
-	Title string             `json:"title" bson:"title"`
-	Key   string             `json:"key" bson:"key"`
-	Value interface{}        `json:"value" bson:"value"`
+	Id     primitive.ObjectID `json:"_id" bson:"_id"`
+	Group  string             `json:"group" bson:"group"`
+	Title  string             `json:"title" bson:"title"`
+	Key    string             `json:"key" bson:"key"`
+	Value  interface{}        `json:"value" bson:"value"`
+	CityId int                `json:"city,omitempty" bson:"city,omitempty"`
 }
