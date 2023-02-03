2cc7cb7373e9c45fe214064a669424c68f01d2e8
mukesh-rara <mukesh@rara.delivery>
2023-02-03 23:22:56 +0530

    batch assignment activity added

--- src/models/driver/BatchAssignmentActivity.go
+++ src/models/driver/BatchAssignmentActivity.go
@@ -3,17 +3,23 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type BatchAssignmentActivity struct {
-	Id               primitive.ObjectID `json:"_id" bson:"_id"`
-	DriverId         string             `json:"driverId" bson:"driverId"`
-	BatchId          string             `json:"batchId" bson:"batchId"`
-	AssignmentId     string             `json:"assignmentId" bson:"assignmentId"`
-	PnSent           int64              `json:"pnSent" bson:"pnSent"`
-	PnReceive        int64              `json:"pnReceive" bson:"pnReceive"`
-	PnClick          int64              `json:"pnClick" bson:"pnClick"`
-	PnReject         int64              `json:"pnReject" bson:"pnReject"`
-	RejectReason     string             `json:"rejectReason" bson:"rejectReason"`
-	AssignmentView   int64              `json:"assignmentView" bson:"assignmentView"`
-	DriverStatus     string             `json:"driverStatus" bson:"driverStatus"`
-	DriverStatusTime string             `json:"driverStatusTime" bson:"driverStatusTime"`
-	CreatedAt        int64              `json:"createdAt" bson:"createdAt"`
+	Id                    primitive.ObjectID `json:"_id" bson:"_id"`
+	DriverId              string             `json:"driverId" bson:"driverId"`
+	BatchId               string             `json:"batchId" bson:"batchId"`
+	AssignmentId          string             `json:"assignmentId" bson:"assignmentId"`
+	PnSent                int64              `json:"pnSent" bson:"pnSent"`
+	PnReceive             int64              `json:"pnReceive" bson:"pnReceive"`
+	PnClick               int64              `json:"pnClick" bson:"pnClick"`
+	PnReject              int64              `json:"pnReject" bson:"pnReject"`
+	RejectReason          string             `json:"rejectReason" bson:"rejectReason"`
+	AssignmentView        int64              `json:"assignmentView" bson:"assignmentView"`
+	DriverStatus          string             `json:"driverStatus" bson:"driverStatus"`
+	DriverStatusTime      string             `json:"driverStatusTime" bson:"driverStatusTime"`
+	CreatedAt             int64              `json:"createdAt" bson:"createdAt"`
+	BatchStartingTime     int64              `json:"batchStartingTime" bson:"batchStartingTime"`
+	TotalDuration         int64              `json:"totalDuration" bson:"totalDuration"`
+	DriverInitialDistance float64            `json:"driverInitialDistance" bson:"driverInitialDistance"`
+	AcceptedAt            int64              `json:"acceptedAt" bson:"acceptedAt"`
+	CompletedAt           int64              `json:"completedAt" bson:"completedAt"`
+	StartPickupAt         int64              `json:"startPickupAt" bson:"startPickupAt"`
 }
