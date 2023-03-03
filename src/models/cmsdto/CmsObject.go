ac93bc7b27c6e1b5e8d17c1f38b285f2265be2c3
mukesh-rara <mukesh@rara.delivery>
2023-03-04 00:13:22 +0530

    -

--- src/models/cmsdto/CmsObject.go
+++ src/models/cmsdto/CmsObject.go
@@ -2,6 +2,7 @@ package cmsdto
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"strings"
 
@@ -129,4 +130,73 @@ func (cmsResponse CmsObject) EvalueateExpressionByDeliveryFee(input float64, ser
 	}
 
 	return true, i
+}
+
+func (cmsResponse *CmsObject) EvalueateExpressionByDeliveryFeeByType(input float64, shippingType string, unit string) (bool, float64, string, string) {
+	//input is the distance
+	serviceFormulaObj := cmsResponse.SLAservices[0].DeliveryFee[0]
+
+	log.Println(serviceFormulaObj)
+	formulaExpression := strings.ReplaceAll(serviceFormulaObj.Formula, "^", "")
+	formulaExpression = strings.ReplaceAll(formulaExpression, "#", "")
+	metricObj := serviceFormulaObj.Metric[0]
+	inputValue := strings.ReplaceAll(metricObj.Ref, "^", "")
+	log.Println("input", input)
+	if input > metricObj.MaxValue || input < metricObj.MinValue {
+		errorMessage := "<SHIPPING_TYPE> should be in the range of <L>-<M> in <UNIT>"
+		errorMessage = strings.Replace(errorMessage, "<SHIPPING_TYPE>", shippingType, 1)
+		errorMessage = strings.Replace(errorMessage, "<L>", fmt.Sprint(metricObj.MinValue), 1)
+		errorMessage = strings.Replace(errorMessage, "<M>", fmt.Sprint(metricObj.MaxValue), 1)
+		errorMessage = strings.Replace(errorMessage, "<UNIT>", unit, 1)
+
+		return false, 0.0, errorMessage, "SH40001"
+	}
+	log.Println("formulaExpression: ", formulaExpression)
+	expression, error := govaluate.NewEvaluableExpression(formulaExpression)
+	if error != nil {
+		log.Println(error)
+		return false, 0.0, "Unknown error in shipping fee calculation", "SH40002"
+	}
+	parameters := make(map[string]interface{}, 8)
+	parameters[inputValue] = input
+
+	for i := 0; i < len(serviceFormulaObj.Variables); i++ {
+		varObj := serviceFormulaObj.Variables[i]
+		variableData := strings.ReplaceAll(varObj.Ref, "#", "")
+		parameters[variableData] = varObj.Value
+
+	}
+
+	log.Println("parameters: ", parameters)
+
+	result, err := expression.Evaluate(parameters)
+	if err != nil {
+		log.Println(err)
+		return false, 0.0, err.Error(), "SH40003"
+	}
+
+	i, ok := result.(float64)
+	log.Println("Checking delivery fee")
+	log.Println(i)
+	if !ok {
+		return false, 0.0, "", "SH40004"
+	}
+
+	return true, i, "", ""
+}
+
+func (cmsObj *CmsObject) SetDeliveryFee(val float64, shippingType string, unit string) (float64, string, bool, string) {
+	//orderWeight := d.PackageDetails.BillableWeight
+
+	//temp fix to be reviewd ??
+	//orderWeight := 0.0
+	// for _, ele := range ItemWeights{
+	// 	orderWeight += ele
+	// }
+
+	st, data, message, code := cmsObj.EvalueateExpressionByDeliveryFeeByType(val, shippingType, unit)
+	// if data > 0 {
+	// 	d.OrderDetails.DeliveryFee = data
+	// }
+	return data, message, st, code
 }
