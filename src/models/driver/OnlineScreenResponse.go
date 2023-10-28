7520a11bb62a4e5ad7464cf68728ed380db6cdaf
mukesh-rara <mukesh@rara.delivery>
2023-10-28 19:08:25 +0530

    Redirect and Polling field added in OnlineScreenResponse struct

--- src/models/driver/OnlineScreenResponse.go
+++ src/models/driver/OnlineScreenResponse.go
@@ -3,14 +3,16 @@ package models
 import "github.com/RaRa-Delivery/rara-ms-models/src/models/order"
 
 type OnlineScreenResponse struct {
-	TodaysEarning   string                    `json:"earningType" bson:"earningType"`
-	Earning         Earning                   `json:"earning" bson:"earning"`
-	Orders          []order.BatchForDriverApp `json:"nodes" bson:"nodes"`
-	Kecamatan       []string                  `json:"kecamatan" bson:"kecamatan"`
-	IsBatchAssigned bool                      `json:"isBatchAssigned" bson:"isBatchAssigned"`
-	IsBatchAccepted bool                      `json:"isBatchAccepted" bson:"isBatchAccepted"`
-	IsFirstStartPickup bool `json:"isFirstStartPickup" bson:"isFirstStartPickup"`
-	StartTime       int64                     `json:"startTime" bson:"startTime"`
-	Duration        int64                     `json:"duration" bson:"duration"`
-	TimeRemaining   int64                     `json:"timeRemaining" bson:"timeRemaining"`
+	TodaysEarning      string                    `json:"earningType" bson:"earningType"`
+	Earning            Earning                   `json:"earning" bson:"earning"`
+	Orders             []order.BatchForDriverApp `json:"nodes" bson:"nodes"`
+	Kecamatan          []string                  `json:"kecamatan" bson:"kecamatan"`
+	IsBatchAssigned    bool                      `json:"isBatchAssigned" bson:"isBatchAssigned"`
+	IsBatchAccepted    bool                      `json:"isBatchAccepted" bson:"isBatchAccepted"`
+	IsFirstStartPickup bool                      `json:"isFirstStartPickup" bson:"isFirstStartPickup"`
+	StartTime          int64                     `json:"startTime" bson:"startTime"`
+	Duration           int64                     `json:"duration" bson:"duration"`
+	TimeRemaining      int64                     `json:"timeRemaining" bson:"timeRemaining"`
+	Redirect           string                    `json:"redirect" bson:"redirect"`
+	Polling            int                       `json:"polling" bson:"polling"`
 }
