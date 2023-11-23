fafb4c6466ebaa06c78fdd93ec4a30e1c7da4460
Mukesh Das <mukeshdas@192.168.254.5>
2023-11-23 12:38:39 +0545

    added coordinates in driverdetails

--- src/models/driver/DriverDetails.go
+++ src/models/driver/DriverDetails.go
@@ -1,8 +1,9 @@
 package models
 
 type DriverDetails struct {
-	Firstname string `json:"firstName" bson:"firstName"`
-	Lastname  string `json:"lastName" bson:"lastName"`
-	Emailid   string `json:"emailId" bson:"emailId"`
-	Mobileno  string `json:"mobileNo" bson:"mobileNo"`
+	Firstname   string    `json:"firstName" bson:"firstName"`
+	Lastname    string    `json:"lastName" bson:"lastName"`
+	Emailid     string    `json:"emailId" bson:"emailId"`
+	Mobileno    string    `json:"mobileNo" bson:"mobileNo"`
+	Coordinates []float64 `json:"coordinates" bson:"coordinates"`
 }
